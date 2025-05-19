edaad23a805c116e89d09c7bcad5ad57d469c0da
osmanfayiaz <113288744+osmanfayiaz@users.noreply.github.com>
2025-05-19 23:16:53 +0500

    Add files via upload
    Question 1

---
+++ Assessment_Q1.sql
@@ -0,0 +1,18 @@
+SELECT 
+    p.owner_id,
+    u.name,
+    SUM(CASE WHEN p.plan_type_id IN (1, 4) AND p.amount > 0 THEN 1 ELSE 0 END) AS savings_count,
+    SUM(CASE WHEN p.plan_type_id NOT IN (1, 4) AND p.amount > 0 THEN 1 ELSE 0 END) AS investment_count,
+    SUM(p.amount) AS total_deposits
+FROM 
+    plans_plan p
+JOIN 
+    users_customuser u ON p.owner_id = u.id
+WHERE 
+    p.is_deleted = 0 AND p.is_archived = 0
+GROUP BY 
+    p.owner_id, u.name
+HAVING 
+    savings_count > 0 AND investment_count > 0
+ORDER BY 
+    total_deposits DESC;
